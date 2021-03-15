a314362f704027923749bb04ee795137ff0f90e8
Julian Maliuk <julian.maljuk@gmail.com>
2021-03-15 21:16:24 +0200

    implement users seed migration

--- src/users/migration/1615143457080-SeedUser.ts
+++ src/users/migration/1615143457080-SeedUser.ts
@@ -1,7 +1,10 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { getRepository, MigrationInterface, QueryRunner } from 'typeorm';
+import { UserSeed } from '../seeds/user.seed';
 
 export class SeedUser1615143457080 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
+  public async up(_: QueryRunner): Promise<any> {
+    await getRepository('users').save(UserSeed);
+  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }
