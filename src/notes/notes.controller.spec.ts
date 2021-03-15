4cbe2d6a0d05096cb3ddd888a40b15b66e0768e9
Julian Maliuk <julian.maljuk@gmail.com>
2021-03-15 21:15:41 +0200

    fix notes tests

--- src/notes/notes.controller.spec.ts
+++ src/notes/notes.controller.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { Note } from './entities/note.entity';
 import { NotesController } from './notes.controller';
 import { NotesService } from './notes.service';
 
@@ -8,7 +12,12 @@ describe('NotesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotesController],
-      providers: [NotesService],
+      providers: [
+        NotesService,
+        UsersService,
+        { provide: getRepositoryToken(Note), useValue: {} },
+        { provide: getRepositoryToken(User), useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<NotesController>(NotesController);
--- src/notes/notes.service.spec.ts
+++ src/notes/notes.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { Note } from './entities/note.entity';
 import { NotesService } from './notes.service';
 
 describe('NotesService', () => {
   let service: NotesService;
+  let repo: Repository<Note>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotesService],
+      providers: [
+        NotesService,
+        UsersService,
+        { provide: getRepositoryToken(Note), useValue: {} },
+        { provide: getRepositoryToken(User), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<NotesService>(NotesService);
+    repo = module.get<Repository<Note>>(getRepositoryToken(Note));
   });
 
   it('should be defined', () => {
