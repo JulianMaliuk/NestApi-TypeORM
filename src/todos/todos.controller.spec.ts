d7fd5a6b950b9d95034a291ab288e1b8dcafa94c
Julian Maliuk <julian.maljuk@gmail.com>
2021-03-20 23:08:12 +0200

    fix tests

--- src/todos/todos.controller.spec.ts
+++ src/todos/todos.controller.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { Todo } from './entities/todo.entity';
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 
@@ -8,7 +12,12 @@ describe('TodosController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodosController],
-      providers: [TodosService],
+      providers: [
+        TodosService,
+        UsersService,
+        { provide: getRepositoryToken(Todo), useValue: {} },
+        { provide: getRepositoryToken(User), useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<TodosController>(TodosController);
--- src/todos/todos.service.spec.ts
+++ src/todos/todos.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { Todo } from './entities/todo.entity';
 import { TodosService } from './todos.service';
 
 describe('TodosService', () => {
   let service: TodosService;
+  let repo: Repository<Todo>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodosService],
+      providers: [
+        TodosService,
+        UsersService,
+        { provide: getRepositoryToken(Todo), useValue: {} },
+        { provide: getRepositoryToken(User), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<TodosService>(TodosService);
+    repo = module.get<Repository<Todo>>(getRepositoryToken(Todo));
   });
 
   it('should be defined', () => {
