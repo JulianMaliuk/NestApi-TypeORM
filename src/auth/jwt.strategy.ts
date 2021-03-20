5b9486db431490438480487108c160e663cd84c8
Julian Maliuk <julian.maljuk@gmail.com>
2021-03-20 12:57:01 +0200

    Implement auth with cookies and url query

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,11 +4,23 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 
+const cookieExtractor = function (req) {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['access_token'];
+  }
+  return token;
+};
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private config: ConfigService, private authService: AuthService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+        ExtractJwt.fromUrlQueryParameter('access_token'),
+        cookieExtractor,
+      ]),
       ignoreExpiration: false,
       secretOrKey: config.get('jwt.secret'),
     });
